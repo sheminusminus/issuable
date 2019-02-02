a36ba8f3cec52f47fd6a31749b9f997770902eaf
Emily Kolar <emkolardev@gmail.com>
2019-02-01 21:56:48 -0600

    auth module, initState, empty reducer

---
+++ src/modules/auth/index.js
@@ -0,0 +1,3 @@
+import * as reducer from './reducer';
+
+export { reducer };
---
+++ src/modules/auth/reducer.js
@@ -0,0 +1,12 @@
+export const name = 'auth';
+
+export const initialState = {
+  token: null,
+};
+
+export function reducer(state = initialState, action = {}) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
