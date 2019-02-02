45af791580fff5143e8172f96e27d7cc7e91f10a
Emily Kolar <emkolardev@gmail.com>
2019-02-01 21:54:01 -0600

    add react redux; boilerplate code for redux store

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 
 import * as serviceWorker from './serviceWorker';
 
+import configureStore from './store';
+
 import './styles.scss';
 import Main from './main';
 
-ReactDOM.render(<Main />, document.getElementById('root'));
+const store = configureStore();
+
+ReactDOM.render(
+  <Main store={store} />,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/main/presenter.jsx
+++ src/main/presenter.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
 
 import './styles.scss';
 
 class Main extends Component {
   render() {
+    const { store } = this.props;
+
     return (
-      <div className="main">
-        Now I am the app!
-      </div>
+      <Provider store={store}>
+        <div className="main">
+          Now I am the app!
+        </div>
+      </Provider>
     );
   }
 }
