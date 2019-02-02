3e5397a5265e20c2bd4493cc26e08628d78c7e6c
Emily Kolar <emkolardev@gmail.com>
2019-02-01 21:49:12 -0600

    replace default app with main, make css sassy

---
+++ src/main/presenter.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './styles.scss';
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="main">
+        Now I am the app!
+      </div>
+    );
+  }
+}
+
+export default Main;
